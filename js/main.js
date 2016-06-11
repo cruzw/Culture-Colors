b8a0e20daf6ae8f95950f5353e9ba1fa18f4ca3e
gcwelborn <gcruzwelborn@gmail.com>
2016-06-11 11:24:13 -0700

    removed counter, added comments
    * removed user counter
    * added comments to help guide those trying to reason with src code
    * moved cultures [array] to culture_data

--- js/main.js
+++ js/main.js
@@ -1,19 +1,26 @@
+// variables found in culture_data.js:
+//    *   cultures [array of culture names],
+//    *   culture_list [object lookup for culture's color data]
+
+// DOM references
 var select_culture = document.getElementById('culture');
 var select_first_value = document.getElementById('first_value');
 var select_second_value = document.getElementById('second_value');
 var shuffle_button = document.getElementById('shuffle');
-var cultures_list = ["African", "Asian", "Chinese", "Eastern European", "Hindu", "Japanese", "Muslim", "Native American", "South American", "Western"];
+
+// initial Cultures and Values chosen at random
 var initial_culture = randomCulture();
 var initial_state = {
   culture: initial_culture,
   first_value: randomValue(initial_culture),
   second_value: randomValue(initial_culture)
 };
+
+// redux, see reducer
 var store = Redux.createStore(reducer);
 
-init();
-
-function init() {
+// initial application setup
+(function init(){
   updateBackgroundGradient();
   populateOptions();
   populateCultures();
@@ -21,8 +28,9 @@ function init() {
   store.subscribe(render);
   console.log('INIT: ', store.getState());
   shuffle_button.addEventListener('click', dispatchShuffle);
-}
+}())
 
+// callback to run after every redux state change
 function render() {
   updateBackgroundGradient();
   populateOptions();
@@ -30,21 +38,21 @@ function render() {
   console.log('RENDER: ', store.getState());
 }
 
+// takes a Culture and two Values, returns them as object/state
 function newState(culture, first_value, second_value) {
-  return {
-    culture: culture,
-    first_value: first_value,
-    second_value: second_value
-  }
+  return { culture, first_value, second_value }
 }
+
 
 function reducer(state, action) {
   console.log('ACTION: ', action);
   switch (action.type) {
-    case '@@redux/INIT':
-      return initial_state
+
+    case '@@redux/INIT': return initial_state
+
     case 'CULTURE_CHANGE':
       return newState(action.culture, randomValue(action.culture), randomValue(action.culture))
+
     case 'VALUE_CHANGE':
       var prev_store = store.getState();
       if (action.side === 'LEFT') {
@@ -54,25 +62,29 @@ function reducer(state, action) {
       } else {
         return state
       }
+
     case 'SHUFFLE':
       var rand_cult = randomCulture();
       return newState(rand_cult, randomValue(rand_cult), randomValue(rand_cult))
+
     default:
       return state;
   }
 }
 
+// updates background colors based on the current state
 function updateBackgroundGradient() {
   var curr_state = store.getState();
   var curr_culture = curr_state.culture;
   var first_value = curr_state.first_value;
   var second_value = curr_state.second_value;
-  var left_color = cultures[curr_culture][first_value]
-  var right_color = cultures[curr_culture][second_value]
+  var left_color = cultures[curr_culture][first_value];
+  var right_color = cultures[curr_culture][second_value];
   var linear_gradient = 'linear-gradient(to right,'+left_color+','+right_color+')';
   document.body.style.background = linear_gradient;
 }
 
+// updates Culture and Values select tags to match user's choice
 function synchronizeSelectInputs() {
   var state = store.getState();
   var curr_culture_keys = Object.keys(cultures[state.culture]);
@@ -81,30 +93,37 @@ function synchronizeSelectInputs() {
   select_culture.selectedIndex = cultures_list.indexOf(store.getState().culture);
 }
 
+// clear Values and new options based on current Culture
 function populateOptions() {
   select_first_value.innerHTML = select_second_value.innerHTML = '';
   var state = store.getState();
   var curr_culture_keys = Object.keys(cultures[state.culture]);
-  curr_culture_keys.forEach(function(value) {
-    select_first_value.add(option_constructor(value));
-    select_second_value.add(option_constructor(value));
-  })
+  curr_culture_keys.forEach(add_option_value_to_select)
 }
 
+// adds a Value option to both select inputs
+function add_option_value_to_select(value) {
+  select_first_value.add(option_constructor(value));
+  select_second_value.add(option_constructor(value));
+}
+
+// constructs an option element given a Value
 function option_constructor(value) {
   var option = document.createElement("option");
   option.text = value;
   option.nodeValue = value;
-  return option;
+  return option
 }
 
+// populates the Culture input with options
 function populateCultures() {
   select_culture.innerHTML = '';
   cultures_list.forEach(function(culture) {
     select_culture.add(option_constructor(culture));
-  })
+  });
 }
 
+// handler for left/right Values, dispatches action for value change
 function valueSelectHandler(event) {
   var SIDE = (event.target.id === 'first_value') ? 'LEFT' : 'RIGHT';
   store.dispatch({
@@ -114,6 +133,7 @@ function valueSelectHandler(event) {
   });
 }
 
+// handler for left/right Values, dispatches action for culture change
 function cultureSelectHandler(event) {
   store.dispatch({
     type: 'CULTURE_CHANGE',
@@ -121,16 +141,19 @@ function cultureSelectHandler(event) {
   });
 }
 
+// handler for shuffle button, shuffles state
 function dispatchShuffle(e) {
   store.dispatch({type: 'SHUFFLE'});
 }
 
+// returns a random Value given a Culture
 function randomValue(culture) {
   var culture_keys = Object.keys(cultures[culture]);
   var rand_key = Math.floor(Math.random() * culture_keys.length);
   return culture_keys[rand_key];
 }
 
+// returns a random Culture
 function randomCulture() {
   var rand_key = Math.floor(Math.random() * cultures_list.length);
   return cultures_list[rand_key];
