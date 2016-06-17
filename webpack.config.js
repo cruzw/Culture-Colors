4921b22c8619a5b5789d64b1e70940ebc076d891
Geoffrey Cruz Welborn <gcruzwelborn@gmail.com>
2016-06-16 20:49:15 -0700

    simplified/expanded build scripts
    * added one for webpack bundling in production (~137kb -> 27kb).
    * added npm start for both css & js workflows concurrently.
    * set warnings to false when uglifying - errors seemed to cause
    unnecessary worry.

--- webpack.config.js
+++ webpack.config.js
@@ -3,29 +3,33 @@ var webpack = require('webpack');
 var path = require('path');
 
 module.exports = {
-  context: path.join(__dirname, "js"),
-  devtool: debug ? "inline-sourcemap" : null,
-  entry: "./main.js",
-  module: {
-    loaders: [
-      {
-        test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel', // 'babel-loader' is also a legal name to reference
-        query: {
-          presets: ['es2015']
-        }
-      }
-    ]
-  },
-  output: {
-    path: __dirname + "/build",
-    filename: "app.min.js"
-  },
-  watch: true,
-  plugins: debug ? [] : [
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
-  ],
+    context: path.join(__dirname, "js"),
+    devtool: debug ? "inline-sourcemap" : null,
+    entry: "./main.js",
+    module: {
+        loaders: [{
+            test: /\.jsx?$/,
+            exclude: /(node_modules|bower_components)/,
+            loader: 'babel', // 'babel-loader' is also a legal name to reference
+            query: {
+                presets: ['es2015']
+            }
+        }]
+    },
+    output: {
+        path: __dirname + "/build",
+        filename: "app.min.js"
+    },
+    watch: true,
+    plugins: debug ? [] : [
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            },
+            mangle: false,
+            sourcemap: false
+        }),
+    ],
 };
